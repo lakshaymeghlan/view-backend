1105b7bf7caa20ad613fb2131c54a054700b4c06
Lakshay Meghlan <lakshaymeghlan6969@gmail.com>
2022-10-06 20:53:49 +0530

    connect to database

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 //helpers
 
 //constants
-const PORT = 8080;
-
+const port = process.env.PORT || 8080;
 //mongoDB connection
 
 // routing
+app.get("/", (req, res) => {
+  res.send("server online");
+});
 
 //server
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((e) => console.log(e));
+
+app.listen(port, () => {
+  console.log(`listen to ${port}`);
+});
