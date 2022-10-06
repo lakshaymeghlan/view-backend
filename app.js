9486eb80080c964d9d4698b3fe19f54f66f2591e
Lakshay Meghlan <lakshaymeghlan6969@gmail.com>
2022-10-06 17:30:31 +0530

    structure of backend

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-import { Express } from "express";
+//npm module
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import cors from "cors";
+
+//helpers
+
+//constants
+const PORT = 8080;
+
+//mongoDB connection
+
+// routing
+
+//server
